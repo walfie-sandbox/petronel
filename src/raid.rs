a0caab18c5827bc7914f6402e5baf384c500bcb5
Walfie <walfington@gmail.com>
2017-07-03 20:47:33 -0400

    Parse boss level, add RaidInfo struct

--- src/raid.rs
+++ src/raid.rs
@@ -5,15 +5,30 @@ use twitter_stream::message::Tweet;
 pub type DateTime = chrono::DateTime<chrono::Utc>;
 pub type TweetId = u64;
 pub type RaidId = String;
+pub type BossLevel = i16;
+pub type BossName = String;
+
+pub struct RaidInfo {
+    pub raid: Raid,
+    pub boss: RaidBoss,
+}
 
 pub struct Raid {
     pub tweet_id: TweetId,
-    pub user: String,
-    pub boss_name: String,
+    pub boss_name: BossName,
     pub raid_id: String,
+    pub user: String,
     pub user_image: Option<String>,
     pub text: Option<String>,
     pub created_at: DateTime,
+    pub language: Language,
+}
+
+pub struct RaidBoss {
+    pub name: BossName,
+    pub level: BossLevel,
+    pub image: Option<String>,
+    pub last_seen: DateTime,
     pub language: Language,
 }
 
@@ -32,6 +47,7 @@ pub enum Language {
     Other,
 }
 
+const DEFAULT_BOSS_LEVEL: BossLevel = 0;
 const GRANBLUE_APP_SOURCE: &'static str =
 r#"<a href="http://granbluefantasy.jp/" rel="nofollow">グランブルー ファンタジー</a>"#;
 
@@ -50,73 +66,102 @@ lazy_static! {
 
     static ref REGEX_IMAGE_URL: Regex = Regex::new("^https?://[^ ]+$")
         .expect("invalid image URL regex");
+
+    static ref REGEX_BOSS_NAME: Regex = Regex::new("\
+        Lv(?:l )?(?P<level>[0-9]+) (.*)\
+    ").expect("invalid boss name regex");
 }
 
-impl Raid {
-    fn parse<'a>(tweet_text: &'a str) -> Option<TweetParts<'a>> {
-        REGEX_JAPANESE
-            .captures(tweet_text)
-            .map(|c| (Language::Japanese, c))
-            .or_else(|| {
-                REGEX_ENGLISH.captures(tweet_text).map(
-                    |c| (Language::English, c),
-                )
-            })
-            .and_then(|(lang, c)| if let (Some(text),
-                                Some(id),
-                                Some(boss),
-                                Some(url)) =
-                (c.name("text"), c.name("id"), c.name("boss"), c.name("url"))
-            {
-                let boss_name = boss.as_str().trim();
-                let url_str = url.as_str();
-
-                if boss_name.contains("http") ||
-                    !url_str.is_empty() && !REGEX_IMAGE_URL.is_match(url_str)
-                {
-                    return None;
-                }
-
-                let t = text.as_str().trim();
-
-                Some(TweetParts {
-                    language: lang,
-                    text: if t.is_empty() { None } else { Some(t) },
-                    raid_id: id.as_str().trim(),
-                    boss_name,
-                })
-            } else {
-                None
-            })
-    }
-
-    pub fn from_tweet(mut tweet: Tweet) -> Option<Raid> {
+impl RaidInfo {
+    pub fn from_tweet(mut tweet: Tweet) -> Option<RaidInfo> {
         if tweet.source != GRANBLUE_APP_SOURCE {
             return None;
         }
 
         let text = ::std::mem::replace(&mut tweet.text, "".into());
 
-        Self::parse(&text).map(move |parsed| {
+        parse_text(&text).map(move |parsed| {
             let user_image = if tweet.user.default_profile_image {
                 None
             } else {
                 Some(tweet.user.profile_image_url_https.into())
             };
 
-            Raid {
+            let level = parse_level(parsed.boss_name).unwrap_or(DEFAULT_BOSS_LEVEL);
+
+            let raid = Raid {
                 tweet_id: tweet.id,
-                user: tweet.user.screen_name.into(),
                 boss_name: parsed.boss_name.into(),
                 raid_id: parsed.raid_id.into(),
+                user: tweet.user.screen_name.into(),
                 user_image,
                 text: parsed.text.map(Into::into),
                 created_at: tweet.created_at,
                 language: parsed.language,
-            }
+            };
+
+            let image = tweet.entities.media.and_then(|mut media| {
+                media.pop().map(|m| m.media_url_https.into())
+            });
+
+            let boss = RaidBoss {
+                name: raid.boss_name.clone(), // TODO: Interned strings
+                level,
+                image,
+                last_seen: raid.created_at,
+                language: raid.language,
+            };
+
+            RaidInfo { raid, boss }
         })
     }
 }
+
+fn parse_text<'a>(tweet_text: &'a str) -> Option<TweetParts<'a>> {
+    REGEX_JAPANESE
+        .captures(tweet_text)
+        .map(|c| (Language::Japanese, c))
+        .or_else(|| {
+            REGEX_ENGLISH.captures(tweet_text).map(
+                |c| (Language::English, c),
+            )
+        })
+        .and_then(|(lang, c)| if let (Some(text),
+                            Some(id),
+                            Some(boss),
+                            Some(url)) =
+            (c.name("text"), c.name("id"), c.name("boss"), c.name("url"))
+        {
+            let boss_name = boss.as_str().trim();
+            let url_str = url.as_str();
+
+            if boss_name.contains("http") ||
+                !url_str.is_empty() && !REGEX_IMAGE_URL.is_match(url_str)
+            {
+                return None;
+            }
+
+            let t = text.as_str().trim();
+
+            Some(TweetParts {
+                language: lang,
+                text: if t.is_empty() { None } else { Some(t) },
+                raid_id: id.as_str().trim(),
+                boss_name,
+            })
+        } else {
+            None
+        })
+}
+
+fn parse_level(name: &str) -> Option<BossLevel> {
+    REGEX_BOSS_NAME.captures(name).and_then(|c| {
+        c.name("level").and_then(
+            |l| l.as_str().parse::<BossLevel>().ok(),
+        )
+    })
+}
+
 
 #[cfg(test)]
 impl<'a> TweetParts<'a> {
@@ -143,7 +188,7 @@ mod test {
     #[test]
     fn parse_ignore_invalid_text() {
         assert_eq!(
-            Raid::parse("#GranblueHaiku http://example.com/haiku.png"),
+            parse_text("#GranblueHaiku http://example.com/haiku.png"),
             None
         );
     }
@@ -153,7 +198,7 @@ mod test {
         // Ignore tweets made via the daily Twitter refresh
         // https://github.com/walfie/gbf-raidfinder/issues/98
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "救援依頼 参加者募集！参戦ID：114514810\n\
                 Lv100 ケルベロス スマホRPGは今これをやってるよ。\
                 今の推しキャラはこちら！　\
@@ -168,7 +213,7 @@ mod test {
     fn parse_ignore_another_daily_refresh() {
         // First two lines are user input
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "救援依頼 参加者募集！参戦ID：114514810\n\
                 Lv100 ケルベロス\n\
                 スマホRPGは今これをやってるよ。\
@@ -184,7 +229,7 @@ mod test {
     fn parse_ignore_extra_space_in_image_url() {
         // First two lines are user input
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "救援依頼 参加者募集！参戦ID：114514810\n\
                 Lv100 ケルベロス\n\
                 https://t.co/5Xgohi9wlE https://t.co/Xlu7lqQ3km",
@@ -196,7 +241,7 @@ mod test {
     #[test]
     fn parse_without_extra_text() {
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "参加者募集！参戦ID：ABCD1234\n\
                 Lv60 オオゾラッコ\n\
                 http://example.com/image-that-is-ignored.png",
@@ -210,7 +255,7 @@ mod test {
         );
 
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "I need backup!Battle ID: ABCD1234\n\
                 Lvl 60 Ozorotter\n\
                 http://example.com/image-that-is-ignored.png",
@@ -227,7 +272,7 @@ mod test {
     #[test]
     fn parse_without_image_url() {
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "Help me 参加者募集！参戦ID：ABCD1234\n\
                 Lv60 オオゾラッコ",
             ),
@@ -240,7 +285,7 @@ mod test {
         );
 
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "Help me I need backup!Battle ID: ABCD1234\n\
                 Lvl 60 Ozorotter",
             ),
@@ -256,7 +301,7 @@ mod test {
     #[test]
     fn parse_with_extra_newline() {
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "参加者募集！参戦ID：ABCD1234\n\
                 Lv60 オオゾラッコ\n",
             ),
@@ -269,7 +314,7 @@ mod test {
         );
 
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "I need backup!Battle ID: ABCD1234\n\
                 Lvl 60 Ozorotter\n",
             ),
@@ -286,7 +331,7 @@ mod test {
     #[test]
     fn parse_extra_text() {
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "Help me 参加者募集！参戦ID：ABCD1234\n\
                 Lv60 オオゾラッコ\n\
                 http://example.com/image-that-is-ignored.png",
@@ -300,7 +345,7 @@ mod test {
         );
 
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "Help me I need backup!Battle ID: ABCD1234\n\
                 Lvl 60 Ozorotter\n\
                 http://example.com/image-that-is-ignored.png",
@@ -317,7 +362,7 @@ mod test {
     #[test]
     fn parse_newlines_in_extra_text() {
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "Hey\n\
                 Newlines\n\
                 Are\n\
@@ -335,7 +380,7 @@ mod test {
         );
 
         assert_eq!(
-            Raid::parse(
+            parse_text(
                 "Hey\n\
                 Newlines\n\
                 Are\n\
