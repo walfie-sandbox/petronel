57ad63bb12888f7f94936e20f24f6d94950a9a1a
Walfie <walfington@gmail.com>
2017-07-15 20:36:18 -0400

    Change Id to be a newtype

--- src/id_pool.rs
+++ src/id_pool.rs
@@ -1,7 +1,9 @@
-type Id = u32;
+#[derive(Clone, Hash, Debug, PartialEq, Eq)]
+pub struct Id(u32);
 
+#[derive(Debug)]
 pub struct IdPool {
-    max_id: Id,
+    max_id: u32,
     available: Vec<Id>,
 }
 
@@ -17,15 +19,18 @@ impl IdPool {
         self.available.pop().unwrap_or_else(|| {
             let id = self.max_id;
 
+            // This will panic when max_id is at u32's max value,
+            // but in practice, there would never be so many
+            // IDs being used at the same time.
             self.max_id += 1;
 
-            id
+            Id(id)
         })
     }
 
     // This should never be called with `id` less than `max_id`
-    pub fn push(&mut self, id: Id) {
-        debug_assert!(id <= self.max_id);
+    pub fn recycle(&mut self, id: Id) {
+        debug_assert!(id.0 <= self.max_id);
         self.available.push(id)
     }
 }
@@ -38,15 +43,15 @@ mod test {
     fn use_id() {
         let mut pool = IdPool::new();
 
-        assert_eq!(pool.get(), 0);
-        assert_eq!(pool.get(), 1);
-        assert_eq!(pool.get(), 2);
+        assert_eq!(pool.get(), Id(0));
+        assert_eq!(pool.get(), Id(1));
+        assert_eq!(pool.get(), Id(2));
 
-        pool.push(1);
-        pool.push(0);
-        assert_eq!(pool.get(), 0);
-        assert_eq!(pool.get(), 1);
-        assert_eq!(pool.get(), 3);
-        assert_eq!(pool.get(), 4);
+        pool.recycle(Id(1));
+        pool.recycle(Id(0));
+        assert_eq!(pool.get(), Id(0));
+        assert_eq!(pool.get(), Id(1));
+        assert_eq!(pool.get(), Id(3));
+        assert_eq!(pool.get(), Id(4));
     }
 }
